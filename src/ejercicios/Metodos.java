60c40c71d15f569577184faa807b46f3bd93733e
AdrianGMQ <adrian.florez.santalla@gmail.com>
2021-03-09 18:57:24 +0100

    Añadido metodo entero a char

--- src/ejercicios/Metodos.java
+++ src/ejercicios/Metodos.java
@@ -6,12 +6,19 @@ public class Metodos {
 		
 	
 		stringConcat("Hola", "Adios");
+		
+		integerToChar(10);
 
 	}
 
 	static String stringConcat (String cadena1, String cadena2) {
 		
 		return cadena1 + " " + cadena2;
+	}
+	
+	static char integerToChar (int numero) {
+		
+		return (char)numero;
 	}
 
 }
