a22c24b0a18415cae82ea5c28ce842f20e683816
AdrianGMQ <adrian.florez.santalla@gmail.com>
2021-03-09 18:48:36 +0100

    Añadido metodo concatenacion 2 Strings

--- src/ejercicios/Metodos.java
+++ src/ejercicios/Metodos.java
@@ -3,8 +3,15 @@ package ejercicios;
 public class Metodos {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+	
+		stringConcat("Hola", "Adios");
 
+	}
+
+	static String stringConcat (String cadena1, String cadena2) {
+		
+		return cadena1 + " " + cadena2;
 	}
 
 }
